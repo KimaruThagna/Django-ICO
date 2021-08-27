4b6b248835d85d2212a806c33222e7bc3fe93fba
Kimaru thagana <macbuntu@localhost.localdomain>
2021-08-27 10:21:30 +0300

    use templates extend to build basic views

--- ico/models.py
+++ ico/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    name = models.CharField(max_length=50)
+    registraion_date = models.DateTimeField(auto_now_add=True)
+    token_balance = models.FloatField(default=0.0)
+    
+    def update_balance(self, amount):
+        self.token_balance += amount
+        self.save()
+    
+class Bid(models.Model):
+    userid = models.ForeignKey(Users, on_delete=models.PROTECT)
+    number_of_tokens = models.IntegerField()
+    bidding_price = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    succesful = models.BooleanField(default=False)
+    
+    def qualify_bid(self):
+        self.succesful = True
+        self.save()
+    
